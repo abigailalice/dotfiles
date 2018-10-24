5c8baa2c3a4822baa0fadd38f784987c3ebfe51e
Abigail Gooding <PastelBlueJellybeans@gmail.com>
2018-10-24 08:00:03 -0700

    mostly work on cron
    bugfix on the cron script, hope its working
    enabled copy mode in tmux

--- src/scripts/cron.sh
+++ src/scripts/cron.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+TMPHOME=$HOME
+
 sudo rm /etc/cron.d/parental-controls
-sudo cp ~/gits/dotfiles/src/cron/parental-controls /etc/cron.d/parental-controls
+echo $TMPHOME/gits/dotfiles/src/cron/parental-controls
+sudo cp $TMPHOME/gits/dotfiles/src/cron/parental-controls /etc/cron.d/
 sudo chown root:root /etc/cron.d/parental-controls
 sudo chmod 700 /etc/cron.d/parental-controls
 
